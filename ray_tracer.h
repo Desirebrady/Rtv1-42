19f2b8a02cfd0ee3f2ad20c353ea823c1eea540b
Desire SHUMBA <dshumba@e4r17p10.wethinkcode.co.za>
2018-08-21 12:24:02 +0200

    hey

--- ray_tracer.h
+++ ray_tracer.h
@@ -7,43 +7,54 @@
 //http://flipcode.com/archives/Raytracing_Topics_Techniques-Part_2_Phong_Mirrors_and_Shadows.shtml
 #define winx 800
 #define winy 800  
+#include <stdio.h>
+#include <math.h>
+#define min(a,b) (((a) < (b)) ? (a) : (b))
 
 //vector
-typedef struct s_vec
+typedef struct      s_vec
 {
-    double  x;
-    double  y;
-    double  z; 
-}               t_vec;
+    float           x;
+    float           y;
+    float           z; 
+}                   t_vec;
 
 //sphere
-typedef struct  s_sphere
+typedef struct      s_sphere
 {
-    t_vec   pos;
-    double  radius;
-}               t_sphere;
+    t_vec           pos;
+    float           radius;
+    int             material;
+}                   t_sphere;
 
 //Colour definition
-typedef struct  s_colour
+typedef struct      s_colour
 {
-    double red;
-    double green;
-    double blue;
-}               t_colour;
+    float           red;
+    float           green;
+    float           blue;
+}                   t_colour;
 
 //ray
-typedef struct  S_ray
+typedef struct      s_ray
 {
-    t_vec   origin;
-    t_vec   dir;
-}               t_ray;
+    t_vec           origin;
+    t_vec           dir;
+}                   t_ray;
 
 // Light definition
-typedef struct s_light
+typedef struct      s_light
 {
-        t_vec pos;
-        t_colour intensity;
-}              t_light;
+        t_vec       pos;
+        t_colour    intensity;
+}                   t_light;
+
+/* Material Definition */
+typedef struct      s_material
+{
+	t_colour        diffuse;
+	float           reflection;
+}                   t_material;
 
 
 #endif
